a847e821439f3ac36155d4d17161ed85c35c9bf3
Carmelo <carmelojsg@gmail.com>
2020-10-28 18:25:17 -0700

    added random shuffle

--- mass_follow_unfollow.py
+++ mass_follow_unfollow.py
@@ -1,6 +1,7 @@
 from instapy import InstaPy
 from instapy import smart_run
 from login import *
+import random
 # get a session!
 session = InstaPy(username=username, password=password)
 # let's go! :>
@@ -16,23 +17,23 @@ with smart_run(session):
     session.set_do_follow(enabled=True, percentage=100)
     session.set_do_like(enabled=True, percentage=100)
     # activity
-    photo_comments = ['Nice shot! @{}',
-                      'I love your profile! @{}',
+    photo_comments = ['Nice shot! :thumbsup:',
+                      'I love your profile!',
                       'Your feed is an inspiration :thumbsup:',
                       'Just incredible :open_mouth:',
-                      'What camera did you use @{}?',
-                      'Love your posts @{}',
-                      'Looks awesome @{}',
-                      'Getting inspired by you @{}',
+                      'What camera did you use? :thumbsup:',
+                      'Love your posts :thumbsup:',
+                      'Looks awesome :thumbsup:',
+                      'Getting inspired by you :thumbsup:',
                       ':raised_hands: Yes!',
-                      'I can feel your passion @{} :muscle:']
+                      'I can feel your passion :muscle:']
     session.set_comments(photo_comments, media='Photo')
     session.set_do_comment(enabled=True, percentage=100)
     session.unfollow_users(amount=75, nonFollowers=True, style="RANDOM", unfollow_after=42 * 60 * 60, sleep_delay=3)
     session.like_by_tags(
-        ['photography', 'blackandwhitephotography', 'naturephotography', 'photographylovers', 'Instaphoto', 'photogram',
+        random.shuffle(['photography', 'blackandwhitephotography', 'naturephotography', 'photographylovers', 'Instaphoto', 'photogram',
          'photographyislife', 'justgoshoot', 'picoftheday', 'photooftheday', 'love', 'nature',
          'sunset', 'sunrise', 'landscape', 'flowers', 'mountains', 'sky', 'lake', 'river', 'ocean',
          'instalike', 'love', 'instagood', 'fashion', 'beautiful', 'friends', 'summer', 'selfie', 'foodk ',
-         'family', 'igers', 'tbt', 'happy'], amount=1000)
+         'family', 'igers', 'tbt', 'happy']), amount=10)
     session.join_pods(topic='general')
